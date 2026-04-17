8580b652bff1d06cfb26058d37cbbcb5ca671032
Oskar971 <ossta007@osloskolen.no>
2026-04-17 08:20:38 +0200

    kkk
    kk

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@ function byttSide(nesteSideId) {
   // Skjul begge sidene først
   document.getElementById("side1").style.display = "none";
   document.getElementById("side2").style.display = "none";
+  document.getElementById("side3").style.display = "none";
+  document.getElementById("side4").style.display = "none";
 
   // Vis siden brukeren valgte
   document.getElementById(nesteSideId).style.display = "block";
