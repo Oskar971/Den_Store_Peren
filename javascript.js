1e984c3f98d735885b22bc81abea44adda9ad539
Oskar971 <ossta007@osloskolen.no>
2026-04-17 08:40:28 +0200

    lol
    lol

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ function byttSide(nesteSideId) {
   document.getElementById("side2").style.display = "none";
   document.getElementById("side3").style.display = "none";
   document.getElementById("side4").style.display = "none";
+  document.getElementById("side5").style.display = "none";
 
   // Vis siden brukeren valgte
   document.getElementById(nesteSideId).style.display = "block";
