be8bc334a62e6d48c6af8e4c6633151d8f07adaa
Oskar971 <ossta007@osloskolen.no>
2026-04-21 09:15:15 +0200

    wow
    wow

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ function byttSide(nesteSideId) {
   //   document.getElementById("side11").style.display = "none";
 
   // Stians måte
-  for (i = 1; i <= 16; i++) {
+  for (i = 1; i <= 17; i++) {
     document.getElementById("side" + i).style.display = "none";
   }
 
